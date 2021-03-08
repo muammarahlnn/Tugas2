ea63d8252f86c9ac00fe954d1fb293c124d8dd9d
muammarahlnn <21muammarahlan@gmail.com>
2021-03-08 22:10:37 +0800

    add ArtikelActivity

--- app/src/main/java/com/ardnn/tugas2/ArtikelActivity.java
+++ app/src/main/java/com/ardnn/tugas2/ArtikelActivity.java
@@ -6,18 +6,20 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class ArtikelActivity extends AppCompatActivity implements View.OnClickListener {
 
-    Button btnArtikel, btnBiodata;
+    LinearLayout btnHome;
+    Button btnBiodata;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_artikel);
 
-        btnArtikel = findViewById(R.id.btn_artikel);
-        btnArtikel.setOnClickListener(this);
+        btnHome = findViewById(R.id.btn_home);
+        btnHome.setOnClickListener(this);
 
         btnBiodata = findViewById(R.id.btn_biodata);
         btnBiodata.setOnClickListener(this);
@@ -26,15 +28,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
-            case R.id.btn_artikel:
-                Intent goToArtikel = new Intent(MainActivity.this, ArtikelActivity.class);
-                startActivity(goToArtikel);
+            case R.id.btn_home:
+                Intent goToMain = new Intent(ArtikelActivity.this, MainActivity.class);
+                startActivity(goToMain);
+                finishAffinity();
                 break;
             case R.id.btn_biodata:
-                Intent goToBiodata = new Intent(MainActivity.this, BiodataActivity.class);
+                Intent goToBiodata = new Intent(ArtikelActivity.this, BiodataActivity.class);
                 startActivity(goToBiodata);
                 break;
-
         }
     }
 }
--- app/src/main/java/com/ardnn/tugas2/BiodataActivity.java
+++ app/src/main/java/com/ardnn/tugas2/BiodataActivity.java
@@ -2,13 +2,16 @@ package com.ardnn.tugas2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.LinearLayout;
 
 public class BiodataActivity extends AppCompatActivity implements View.OnClickListener {
 
     LinearLayout btnHome;
+    Button btnArtikel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,13 +20,22 @@ public class BiodataActivity extends AppCompatActivity implements View.OnClickLi
 
         btnHome = findViewById(R.id.btn_home);
         btnHome.setOnClickListener(this);
+
+        btnArtikel = findViewById(R.id.btn_artikel);
+        btnArtikel.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.btn_home:
-                finish();
+                Intent goToMain = new Intent(BiodataActivity.this, MainActivity.class);
+                startActivity(goToMain);
+                finishAffinity();
+                break;
+            case R.id.btn_artikel:
+                Intent goToArtikel = new Intent(BiodataActivity.this, ArtikelActivity.class);
+                startActivity(goToArtikel);
                 break;
         }
     }
